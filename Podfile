b2f3a861c91751ec08c850b9c994426aa37b54d3
lex <lex@users.noreply.github.com>
2018-06-08 22:39:46 -0700

    parse top stories

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'juuhn' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for juuhn
+  pod 'Alamofire', '~> 4.7'
+  pod 'Kanna', '~> 4.0.0'
+end
--- juuhn/ViewController.swift
+++ juuhn/ViewController.swift
@@ -7,19 +7,52 @@
 //
 
 import UIKit
+import Alamofire
+import Kanna
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        scrapeHN();
     }
-
+    
+    func scrapeHN() {
+        let url = "https://news.ycombinator.com";
+        
+        Alamofire.request(url).responseString { response in
+            if let html = response.result.value {
+                do {
+                    try self.parseHTML(html: html)
+                } catch {
+                    print("rip")
+                }
+            }
+        }
+    }
+    
+    func parseHTML(html: String) throws {
+        var stories: [(String, String, String, String, String)] = []
+        
+        let doc = try Kanna.HTML(html: html, encoding: String.Encoding.utf8)
+        var titles: [String?] = doc.css(".storylink").map { title in title.text }
+        
+        let subtexts = doc.css(".subtext")
+        for subtext in subtexts {
+            if let title = titles.removeFirst(),
+                let score = subtext.css(".score").first?.text,
+                let age = subtext.css(".age").first?.text,
+                let user = subtext.css(".hnuser").first?.text,
+                let comments = subtext.css("a").reversed().first?.text {
+                stories.append((title, score, age, user, comments))
+            }
+        }
+        
+        print(stories)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
